ca7b1d3d23601d4dec017116e1809159eb6a09a3
Amo-xm <335452774@qq.com>
2022-03-02 12:55:18 +0800

    jsp

--- Desktop/jsp-onlineShop/service/app/middleware/adminauth.js
+++
@@ -1,9 +0,0 @@
-module.exports = options => {
-    return async function adminauth(ctx, next) {
-        if (this.ctx.openId) {
-            await next()
-        } else {
-            ctx.body = { data: '没有登录' }
-        }
-    }
-}
